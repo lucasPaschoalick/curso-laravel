831212a5d43c26a80d798aa498570f6f0dee381a
Lucas Paskua <80064601+paskua4@users.noreply.github.com>
2023-02-09 15:55:11 -0300

    Implementação de inserção de imagens e ajustes css

--- curso_laravel/resources/views/events/create.blade.php
+++ curso_laravel/resources/views/events/create.blade.php
@@ -3,7 +3,36 @@
 @section('title', 'Criar Evento')
 
 @section('content')
+    <div id="event-create-container" class="col-md-6 offset-md-3">
+        <h1 class="mb-4">Crie um evento</h1>
+        <form action="/events" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group mb-4">
+                <label for="image">Imagem do Evento:</label>
+                <input type="file" id="image" name="image" class="from-control-file">
+            </div>
+            <div class="form-group mb-4">
+                <label for="title">Evento:</label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+            </div>
+            <div class="form-group mb-4">
+                <label for="title">Cidade:</label>
+                <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
+            </div>
+            <div class="form-group mb-4">
+                <label for="title">O evento é público ou privado?</label>
+                <select name="private" id="private" class="form-control">
+                    <option value="0">Público</option>
+                    <option value="1">Privado</option>
+                </select>                
+            </div>
+            <div class="form-group mb-4">
+                <label for="title">Descrição:</label>
+                <textarea name="description" id="description" class="form-control" placeholder="Descreva as atividades do evento"></textarea>
+            </div>
+            <input type="submit" class="btn btn-primary" value="Criar Evento">
+        </form>
 
-    <h1>Crie um evento</h1>    
+    </div>
 
 @endsection
--- curso_laravel/resources/views/welcome.blade.php
+++ curso_laravel/resources/views/welcome.blade.php
@@ -3,12 +3,33 @@
 @section('title', 'HDC Events')
 
 @section('content')
-
-    <h1>Título</h1>
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um evento</h1>
+        <form action="">
+            <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+        </form>
+    </div>
     
-    <img src="/img/banner.jpg" alt="Banner">
+    <div id="events-container" class="col-md-12">
+        <h2>Próximos Eventos</h2>
+        <p>Veja os eventos dos próximos dias</p>
+        <div id="cards-container" class="row">
+            @foreach($events as $event)
+                <div class="card col-md-3">
+                    <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
+                    <div class="card-body">
+                        <p class="card-date">09/02/2023</p>
+                        <h5 class="card-title">{{ $event->title }}</h5>
+                        <p class="card-participants">X Participantes</p>
+                        <a href="#" class="btn btn-primary">Saber mais</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+    <!-- <img src="/img/banner.jpg" alt="Banner">
     @foreach($events as $event)
         <p>{{ $event->title }} -- {{ $event->description }}</p>
-    @endforeach
+    @endforeach -->
 
 @endsection
